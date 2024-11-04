e7f7d2788b59a537ce4861c92bcfea972e278c87
Apurbokabbo <apurbokabbo1999@gmail.com>
2024-11-04 12:27:40 +0600

    email.js file update

--- js/email.js
+++ js/email.js
@@ -1,8 +1,5 @@
 // Initialize EmailJS and set up form submission handling
 function initializeEmailJS(e) {
-  // e.preventDefault();
-  console.log("email testing !!!");
-  // Initialize EmailJS with your public key
   emailjs.init("OPgp3Fon1tDaszFAx");
 
   // Form submission handler
@@ -11,7 +8,7 @@ function initializeEmailJS(e) {
     .addEventListener("submit", function (e) {
       e.preventDefault();
 
-       // Get form fields
+      // Get form fields
       const name = document.getElementById("name").value.trim();
       const email = document.getElementById("email").value.trim();
       const message = document.getElementById("message").value.trim();
@@ -33,19 +30,11 @@ function initializeEmailJS(e) {
       }
 
       const templateParams = {
-        to_name: "kabbo", // Static value or use form data if needed
+        to_name: "Apurbo Kabbo", // Static value or use form data if needed
         from_name: document.getElementById("name").value,
         message: document.getElementById("message").value,
         reply_to: document.getElementById("email").value,
       };
-
-      // Prepare the parameters to send
-      // const templateParams = {
-      //   to_name: name,
-      //   from_name: email,
-      //   message: message,
-      //   reply_to: email,
-      // };
 
       // emailjs.send("service_4eaeg8k", "template_i19ur1d", {
       //   to_name: "kabbo",
@@ -53,22 +42,6 @@ function initializeEmailJS(e) {
       //   message: "hello body",
       //   reply_to: "test@gmail.com",
       // });
-
-    
-
-      //   emailjs.send("service_4eaeg8k", "template_i19ur1d", templateParams).then(
-      //     function (response) {
-      //       statusMessage.textContent = "Message sent successfully!";
-      //       document.getElementById("contactForm").reset(); // Clear form fields
-      //     },
-      //     function (error) {
-      //       errorMessage.textContent =
-      //         "Failed to send message. Please try again.";
-      //       console.error("Error:", error);
-      //     }
-      //   );
-      // });
-
 
       emailjs.send("service_4eaeg8k", "template_i19ur1d", templateParams).then(
         function (response) {
